a49ac29b0721163dcce92ba2d280eca1178c17a7
Mathurkarishma <76825109+Mathurkarishma@users.noreply.github.com>
2021-01-01 17:45:02 -0500

    Add files via upload
    Python code and text files.

---
+++ KM-Romeo.py
@@ -0,0 +1,32 @@
+#DATA 620
+#Assignment 11.2
+#Professor Menta
+#Written by Karishma Mathur
+#December 8, 2019
+
+#This program will ask the user for a file and extract separate words line by line
+#It will add only unique words to a list and sort it alphabetically
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+    #If the user inputted an invalid file name, program will exit
+list = []
+#List begins empty
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in list:
+            list.append(word)
+            #Only adding unique words to list
+        else:
+            continue
+list.sort()
+#Alphabetical sort
+print(list)
+#Print list
+
+#End of script
